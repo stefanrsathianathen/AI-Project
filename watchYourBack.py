b0267ff8cf1753ce4faa3ee8d153edcea1ca24b7
TheCop03 <33970609+TheCop03@users.noreply.github.com>
2018-03-22 16:52:36 +1100

    added gameName variable

--- watchYourBack.py
+++ watchYourBack.py
@@ -5,12 +5,18 @@ import board as b
 #creates the board
 board = b.Board()
 
-#reads all the input 
+#reads all the input
 no_of_lines = 8
 lines = [[]]*no_of_lines
 for i in range(no_of_lines):
     lines[i]=list(input().replace(' ', ''))
 
+gameName = input()
+
 #populates the board with the pieces
 board.createBoard(lines)
-board.findNumberOfMoves()
+
+if (gameName == "Moves"):
+    board.findNumberOfMoves()
+#elif (gameName == "Massacre"):
+    # do stuff
