51e96805682fdd31a0101dc6530b8a017678f282
TheCop03 <33970609+TheCop03@users.noreply.github.com>
2018-03-22 16:52:56 +1100

    did some pep8 stuff

--- board.py
+++ board.py
@@ -9,12 +9,12 @@ class Board():
 		pass
 
 
-	def createBoard(self,boardInput):
+	def createBoard(self, boardInput):
 		'''Loop through text input and put into board by creating cell
 		then the cell will create a piece if it is occupied by a @ or O or X'''
-		for y in range(0,len(boardInput)):
-			for x in range(0,len(boardInput[y])):
-				self.board[y].append(self.createCell(x,y,boardInput[y][x]))
+		for y in range(0, len(boardInput)):
+			for x in range(0, len(boardInput[y])):
+				self.board[y].append(self.createCell(x, y, boardInput[y][x]))
 		self.viewBoard()
 
 
@@ -58,17 +58,15 @@ class Board():
 		print(blackMoves)
 
 
-	def createCell(self,x,y,occupiedBy):
-		"""Creates a cell. If the cell is occupied by a @ or ) or X it will create a piece that lies on top of the cell"""
-		return c.Cell(x,y,occupiedBy)
+	def createCell(self, x, y, occupiedBy):
+		"""Creates a cell. If the cell is occupied by a @ or ) or
+			X it will create a piece that lies on top of the cell"""
+		return c.Cell(x, y, occupiedBy)
 
 	# def destorypiece():
 	# 	destorypiec
 
-	def move(self,oldX,oldY,newX,newY):
+	def move(self, oldX, oldY, newX, newY):
 		'''Moves piece on board'''
 	 #	check if player can move (NEED VALID MOVE)
 		self.board[oldY][oldX].changePiece(self.board[newY][newX])
-
-	# def shrinkboard():
-	# 	shrinkboard
