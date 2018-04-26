8e2c6541d461678f778b6e089da9a6c920297957
TheCop03 <33970609+TheCop03@users.noreply.github.com>
2018-04-26 14:15:29 +1000

    print states

--- mainTest.py
+++ mainTest.py
@@ -1,30 +1,92 @@
 import Board as b
+import gameTree as g
+import copy
 
 myBoard = b.Board()
+piece = "W"
 
 myBoard.placePiece((1,4), "white")
-myBoard.placePiece((4,3), "white")
-myBoard.placePiece((1,5), "black")
+# myBoard.placePiece((4,3), "white")
+# myBoard.placePiece((1,5), "black")
+#
+# for y in range(0, 8):
+#     for x in range (0, 8):
+#         print(myBoard.board[y][x], end = " ")
+#     print("\n")
+# print(myBoard.pieces)
+#
+# #myBoard.move(((2,5), (2,6)))
+# myBoard.shrink_board()
+#
+# for y in range(0, 8):
+#     for x in range (0, 8):
+#         print(myBoard.board[y][x], end = " ")
+#     print("\n")
+# print(myBoard.pieces)
+#
+# myBoard.shrink_board()
+#
+# for y in range(0, 8):
+#     for x in range (0, 8):
+#         print(myBoard.board[y][x], end = " ")
+#     print("\n")
+# print(myBoard.pieces)
 
-for y in range(0, 8):
-    for x in range (0, 8):
-        print(myBoard.board[y][x], end = " ")
-    print("\n")
-print(myBoard.pieces)
+def gameStates(x, y):
+    '''create the possible game states for current piece'''
+    moves = []
+    ''' Move to the right '''
+    if ((myBoard.isValidMove(x, y, x + 1, y)) or
+    (myBoard.isValidMove(x, y, x + 2, y))):
+        if(myBoard[y][x+1] != myBoard[y][x]):
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x+1,y))))
+        else:
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x+2,y))))
+    ''' Move to the left '''
+    if ((myBoard.isValidMove(x, y, x - 1, y)) or
+        (myBoard.isValidMove(x, y, x - 2, y))):
+        if(myBoard[y][x-1] != myBoard[y][x]):
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x-1,y))))
+        else:
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x-2,y))))
+    ''' Move down '''
+    if ((myBoard.isValidMove(x, y, x, y + 1)) or
+            (myBoard.isValidMove(x, y, x, y + 2))):
+        if(myBoard[y+1][x] != myBoard[y][x]):
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x,y+1))))
+        else:
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x,y+2))))
+    ''' Move up '''
+    if ((myBoard.isValidMove(x, y, x, y - 1)) or
+            (myBoard.isValidMove(x, y, x, y - 2))):
+        if(myBoard[y-1][x] != myBoard[y][x]):
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x,y-1))))
+        else:
+            moves.append(g.GameNode(copy.deepcopy(self.board).move((x,y),(x,y-2))))
+    return moves
 
-#myBoard.move(((2,5), (2,6)))
-myBoard.shrink_board()
+def main():
+    myBoard = b.Board()
+    piece = "W"
 
-for y in range(0, 8):
-    for x in range (0, 8):
-        print(myBoard.board[y][x], end = " ")
-    print("\n")
-print(myBoard.pieces)
+    myBoard.placePiece((1,4), "white")
 
-myBoard.shrink_board()
+    ''' Moving Phase '''
+    parentNode = g.GameNode(myBoard)
 
-for y in range(0, 8):
-    for x in range (0, 8):
-        print(myBoard.board[y][x], end = " ")
-    print("\n")
-print(myBoard.pieces)
+    for x in range(0,len(myBoard.board)):
+        for y in range(0,len(myBoard.board)):
+            if myBoard.board[y][x] == piece:
+                states = gameStates(x,y)
+                for state in states:
+                    parentNode.addChild(state.defineParent(parentNode))
+
+    for gameState in parentNode.children:
+        for x in range(0,len(gameState.board)):
+            for y in range(0,len(gameState.board)):
+                if gameState.board[y][x] != self.piece and gameState.board[y][x]:
+                    opponentStates = self.gameStates(x,y)
+                    for state in states:
+                        gameState.addChild(state.defineParent(gameState))
+
+main()
