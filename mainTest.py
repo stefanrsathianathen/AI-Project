60935dfb87c3a018c261333d86579dc62c37492e
TheCop03 <33970609+TheCop03@users.noreply.github.com>
2018-04-28 18:48:21 +1000

    made gamesStates() a bit more simpler. Minmax works perfectly.

--- mainTest.py
+++ mainTest.py
@@ -1,69 +1,37 @@
 import Board as b
 import gameTree as g
 import copy
+from random import randint
 
 myBoard = b.Board()
-# myBoard.placePiece((4,3), "white")
-# myBoard.placePiece((1,5), "black")
-#
-# for y in range(0, 8):
-#     for x in range (0, 8):
-#         print(myBoard.board[y][x], end = " ")
-#     print("\n")
-# print(myBoard.pieces)
-#
-# #myBoard.move(((2,5), (2,6)))
-# myBoard.shrink_board()
-#
-# for y in range(0, 8):
-#     for x in range (0, 8):
-#         print(myBoard.board[y][x], end = " ")
-#     print("\n")
-# print(myBoard.pieces)
-#
-# myBoard.shrink_board()
-#
-# for y in range(0, 8):
-#     for x in range (0, 8):
-#         print(myBoard.board[y][x], end = " ")
-#     print("\n")
-# print(myBoard.pieces)
 
-def gameStates(x, y):
+def gameStates(x, y, rootBoard):
     '''create the possible game states for current piece'''
     moves = []
-    for dx, dy, dx1, dy1 in [(1, 0, 2, 0),(-1, 0, -2, 0),(0, 1, 0, -2),(0, -1, 0, -2)]:
-        try:
-            if myBoard.isValidMove(((x,y),(x+dx,y+dy))) or myBoard.isValidMove(((x,y),(x+dx1,y+dy1))):
-                if(myBoard.board[y+dy][x+dx] != myBoard.board[y][x]):
-                    moves.append(g.GameNode(copy.deepcopy(myBoard).move(((x,y),(x+dx,y+dy))),((x,y),(x+dx,y+dy))))
-                else:
-                    moves.append(g.GameNode(copy.deepcopy(myBoard).move(((x,y),(x+dx1,y+dy1))),((x,y),(x+dx1,y+dy1))))
-        except IndexError:
-            pass
 
-    # for l in moves:
-    #     for y in range(0, 8):
-    #         for x in range (0, 8):
-    #             print(l.board[y][x], end = " ")
-    #         print("\n")
-    #     print("\n")
+    for dx, dy in [(1,0), (0,1), (-1,0), (0,-1), (2,0), (0,2), (-2,0), (0,-2)]:
+        try:
+            if rootBoard.isValidMove(((x, y), (x + dx, y + dy))):
+                tmpBoard = copy.deepcopy(rootBoard)
+                tmpBoard.move(((x, y), (x + dx, y + dy)))
+                moves.append(g.GameNode(tmpBoard, ((x, y), (x + dx, y + dy))))
+        except IndexError:
+            #pass
+            continue
 
     return moves
 
 def value():
-    import random 
-    random.seed(4)
-    return random.randint(-2,6)
+    return randint(-20,60)
 
 def minMax(parentNode):
     for x in parentNode.children:
-        minValue = 10000143135134
+        minValue = float('inf')
         for u in x.children:
             if u.value < minValue:
                 minValue = u.value
         x.value = minValue
-    maxValue = -1234519583724
+    maxValue = float("-inf")
     maxNode = None
     for l in parentNode.children:
         if l.value > maxValue:
@@ -74,38 +42,57 @@ def minMax(parentNode):
 
 def main():
     piece = "W"
+    opponentPiece = "B"
 
-    myBoard.placePiece((2,4), "white")
-    myBoard.placePiece((3,5), "white")
-    myBoard.placePiece((2,5), "black")
-    myBoard.placePiece((6,3), "black")
+    ''' place 12 pieces for both '''
+    for i in range(0, 12):
+        while True:
+            x, y = randint(0,7), randint(0,7)
+            if (x, y) not in myBoard.placeBanList:
+                myBoard.placePiece((x, y), "white")
+                break
+        while True:
+            x, y = randint(0,7), randint(0,7)
+            if (x, y) not in myBoard.placeBanList:
+                myBoard.placePiece((x, y), "black")
+                break
 
     ''' Moving Phase '''
     parentNode = g.GameNode(copy.deepcopy(myBoard),None)
-    
 
+
+    lvl2node = 0
+    lvl3node = 0
     for x in range(0,len(myBoard.board)):
         for y in range(0,len(myBoard.board)):
             if myBoard.board[y][x] == piece:
-                states = gameStates(x,y)
+                states = gameStates(x, y, myBoard)
                 for state in states:
                     state.defineParent(parentNode)
                     parentNode.addChild(state)
+                    #state.board.printBoard()
+                    #print("\n", state.move)
+                    lvl2node += 1
 
-    
+
     for gameState in parentNode.children:
-        for x in range(0,len(gameState.board)):
-            for y in range(0,len(gameState.board)):
-                if gameState.board[y][x] != piece and gameState.board[y][x] != "X" and gameState.board[y][x] != "-":
-                    opponentStates = gameStates(x,y)
-                    for state in states:
+        for x in range(0, 8):
+            for y in range(0, 8):
+                if gameState.board.board[y][x] == opponentPiece:
+                    opponentStates = gameStates(x,y, gameState.board)
+                    for state in opponentStates:
                         state.defineParent(gameState)
                         state.value = value()
                         gameState.addChild(state)
+                        #state.board.printBoard()
+                        lvl3node += 1
 
     nextMove = minMax(parentNode)
     myBoard.printBoard()
-    print(nextMove.move)
+    print(nextMove.move, lvl2node, lvl3node)
+
+    # minMax = m.MiniMax(parentNode)
+    # minMax.minimax(parentNode)
 
 
 main()
