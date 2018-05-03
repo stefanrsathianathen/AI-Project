5b307e3a54318b416b88feef1744730a79802808
Stefan Sathianathen <stefansathianathen@Stefans-MacBook-Pro.local>
2018-05-03 21:51:09 +1000

    i think heursitc is nice

--- Player.py
+++ Player.py
@@ -60,7 +60,7 @@ class Player():
                     if gameState.board.board[y][x] == self.opponentPiece:
                         opponentStates = self.gameStates(x, y, gameState.board)
                         for state in opponentStates:
-                            state.value = self.value(state.board)
+                            state.value = self.score(state.board)
                             state.defineParent(gameState)
                             gameState.addChild(state)
 
@@ -77,11 +77,11 @@ class Player():
             if (type(action[0]) == int):
                 ''' Opponent placed a piece '''
                 self.board.placePiece(action, self.opponentColour)
-                self.board.printBoard()
+                # self.board.printBoard()
             else:
                 ''' Opponent moved a piece '''
                 self.board.move(action)
-                self.board.printBoard()
+                # self.board.printBoard()
 
         self.board.n_turns += 1
 
@@ -103,26 +103,24 @@ class Player():
                 continue
         return moves
 
-    def value(self,board):
+    def score(self,board):
         value = 0
         if board.board.count(self.piece) > board.board.count(self.opponentPiece):
-            value += 1
+            value += board.board.count(self.piece)*board.board.count(self.opponentPiece)
         else:
-            value -= 1
+            value -= board.board.count(self.piece)*board.board.count(self.opponentPiece)
         for x in range(0,len(board.board)):
             for y in range(0,len(board.board)):
                 if board.board[y][x] == self.piece:
                     #control the very centre of board for longivity
-                    if x < 2 or x > 5:
-                        value -= 1
-                    else:
-                        value += 5
-                    if y < 2 or y > 5:
-                        value -= 1
-                    else:
-                        value += 5
-
-
+                    # if x < 2 or x > 5:
+                    #     value -= 10 * x*y
+                    # else:
+                    #     value += 10 * x*y
+                    # if y < 2 or y > 5:
+                    #     value -= 10 * y*x
+                    # else:
+                    #     value += 10 * y*x
 
                     movable = True
                     for dx, dy in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
@@ -130,44 +128,39 @@ class Player():
                         #not helpful if the same pieces are right next to each other
                         try:
                             if board.board[y + dy][x + dx] == self.piece:
-                                value -= 5
+                                value -= 100 * (x + dx) + 50 * (y + dy)
                         except IndexError:
                             pass
                         #Its good to have control of cells in corners for easy kills
                         try:
                             if board.board[y + dy][x + dx] == "X":
-                                value += 5
+                                value += 10 * (x + dx) + 5 * (y + dy)
                         except IndexError:
                             pass
                         # Want to move closer to other pieces so you can eliminate them
                         try:
                             if board.board[y + dy][x + dx] == self.opponentPiece:
-                                value += 1
+                                value += 10 * (x + dx) + 5 * (y + dy)
                         except IndexError:
                             pass
-                        #Its good if the piece can move in all directions after moving
+                        #its good if a piece has a valid move after moving
                         try:
                             if not board.isValidMove(((x, y), (x + dx, y + dy))):
                                 movable = False
                         except IndexError:
-                            value -= 5
-                        #not good if the new if a move will cant move again
-                        try:
-                            board.board[y + dy][x + dx]
-                        except IndexError:
-                            value -= 1
+                            value -= 100 * (x*y)
                     if movable:
-                        value += 5
+                        value += 10 * (x*y)
                     else:
-                        value -= 1
+                        value -= 100 * (x*y)
                     #check diagonals
                     for dx, dy in [(1, 1), (-1, 1), (1, -1), (-1, -1)]:
                         try:
                             # you dont want pieces to close togther
                             if board.board[y + dy][x + dx] == self.piece:
-                                value -= 1
+                                value -= 100 * (x + dx) + 50 * (y + dy)
                         except IndexError:
-                            value -= 1
+                            value -= 100 * (x + dx) + 50 * (y + dy)
                         try:
                             # you could work to elimate this piece
                             if board.board[y + dy][x + dx] == self.opponentPiece:
@@ -175,18 +168,18 @@ class Player():
                                     if (x+dx,y+dy) == (dx+dx1,dy+dy1):
                                         continue
                                     if board.board[dy+dy1][dx+dx1] == self.piece:
-                                        value += 1
+                                        value += 10 * (dx + dx1) + 5 * (dy + dy1)
                                     elif board.board[dy+dy1][dx+dx1] == self.opponentPiece:
-                                        value -= 1
+                                        value -= 100 * (dx + dx1) + 50 * (dy + dy1)
                         except IndexError:
-                            value -= 1
+                            value -= 100 * (x + dx) + 50 * (y + dy)
                     for dx, dy in [(2, 0), (0, 2), (0, -2), (-2, 0)]:
                         #looking for close
                         try:
                             if board.board[y + dy][x + dx] == self.piece:
-                                value += 1
+                                value += 10 * (x + dx) + 5 * (y + dy)
                         except IndexError:
-                            value += 1
+                            value += 10 * (x + dx) + 5 * (y + dy)
         return value
 
 
